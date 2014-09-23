2e54a30aa1844a91865f433fe0d5ea91979e8bf5
afly <aflymail@gmail.com>
2014-09-23 22:35:13 +0800

    引入require机制

--- show.php
+++ show.php
@@ -6,10 +6,8 @@
 </head>
 <body>
   <?php        
-    mysql_connect('localhost','demo','demo') or die('can`t work');
-    mysql_query("SET NAMES utf8");    
-    mysql_select_db('phpdemo');
-
+    require_once './inc/db.php';
+    
     $id = $_GET['id'] ;
     $sql = 'select * from posts where id = ' . $id;
     $query = mysql_query($sql);
