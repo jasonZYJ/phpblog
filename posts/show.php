f4b9dca4cab3979c043b6021b71af9b51b090fcb
openxyz <aflymail@gmail.com>
2014-10-12 22:29:30 +0800

    增加评论功能

--- posts/show.php
+++ posts/show.php
@@ -17,5 +17,30 @@
   <h1><?php echo $post->id; ?> : <?php echo $post->title; ?>  </h1>
   <p><?php echo $post->body; ?></p>
 
+  我要评论
+  <form action="../comments/save.php" method="post">
+    <input type="hidden" name='post_id' value='<?php echo $_GET['id']; ?>'/>
+    <label for="title">title</label>
+    <input type="text" name="title" value="" />
+    <br/>
+    <label for="body">body</label>
+    <textarea name="body"></textarea>
+    <br/>
+    <input type="submit" value="提交" />
+  </form>
+
+  <ol>
+  <?php
+    $query = $db->query('select * from comments where post_id = ' . $_GET['id']);
+    while ( $comment =  $query->fetchObject() ) {
+      ?>
+          <li>
+            <h4><?php echo $comment->title; ?></h4>
+            <span><?php echo date('Y-m-d',strtotime($comment->created_at));?></span>
+            <p><?php echo $comment->body; ?></p>            
+          </li> 
+ 
+    <?php  } ?>
+
 </body>
 </html>
