404866862744b078222a2bbf28e2d1d90337fa52
openxyz <aflymail@gmail.com>
2014-10-22 22:23:25 +0800

    将代码与模板分离,以posts为例

---
+++ app/view/post/index.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>首页 - 博客</title>
+</head>
+
+<body>
+  @include('notice')
+  @if (is_login())
+    <p>欢迎你: {{current_user()->name}}</p>
+  @else
+    <a href='sessions/new.php'>login</a>
+  @endif  
+  <table border=1>
+    <caption><h1>帖子列表</h1></caption>
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>标题</th>
+        <th>创建日期</th>
+        <th>操作</th>        
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        //need to be refact as $pager->result();
+        foreach ($pager->query($_GET['page'])->get() as $post) {
+      ?>
+          <tr>
+            <td>{{ $post->id }}</td>
+            <td><a href="show.php?id={{$post->id}}">{{$post->title}}</a></td>
+            <td>{{ date('Y-m-d',strtotime($post->created_at)) }}</td>
+            <td> 
+              <a href="edit.php?id={{$post->id}}">改</a> 
+              <a href="delete.php?id={{$post->id}}">删</a> 
+            </td>        
+          </tr> 
+ 
+      <?php  } ?>
+  
+    </tbody>
+  </table>
+  <a href="new.php">新增</a>
+
+  {{ $pager->nav_html() }}
+</body>
+</html>
+
+
--- posts/index.php
+++ posts/index.php
@@ -11,55 +11,10 @@ require_once '../inc/session.php';
 
 //pass a eloquent relation
 $pager = new Pager(PostModel::select('*'));
-$posts = $pager->query($_GET['page'])->get();
+//$posts = $pager->query($_GET['page'])->get();
 //$pager2 = new Pager('select * from users ',2,'page2');
 //$query2 = $pager2->query($_GET['page2']);
+
+require_once '../inc/blade.php';
+echo $blade->view()->make('post.index',['pager'=>$pager]);
 ?>
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>首页 - 博客</title>
-</head>
-
-<body>
-  <?php 
-    require_once '../inc/blade.php';
-    echo $blade->view()->make('notice');
-  ?>
-  <?php if(is_login()) echo '当前用户: ' . current_user()->name ;?>
-  <table border=1>
-    <caption><h1>帖子列表</h1></caption>
-    <thead>
-      <tr>
-        <th>id</th>
-        <th>标题</th>
-        <th>创建日期</th>
-        <th>操作</th>        
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-        foreach ($posts as $post) {
-      ?>
-          <tr>
-            <td><?php echo $post->id; ?></td>
-            <td><a href="show.php?id=<?php print $post->id; ?>"><?php echo $post->title; ?></a></td>
-            <td><?php echo date('Y-m-d',strtotime($post->created_at));?></td>
-            <td> 
-              <a href="edit.php?id=<?php echo $post->id; ?>">改</a> 
-              <a href="delete.php?id=<?php echo $post->id; ?>">删</a> 
-            </td>        
-          </tr> 
- 
-      <?php  } ?>
-  
-    </tbody>
-  </table>
-  <a href="new.php">新增</a>
-
-  <?php echo $pager->nav_html(); ?> 
-</body>
-</html>
-
-
